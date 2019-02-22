6b1983b3856218fe6b66e589e25480a4eccd8d10
Lillian Dixon <dixon.lillian92@gmail.com>
2019-02-22 14:05:28 -0700

    menu page

--- src/components/pages/menu.js
+++ src/components/pages/menu.js
@@ -2,68 +2,72 @@ import React from 'react'
 
 export default function() {
     return (
-        <div class="menu-wrapper">
-            <div ClassName = "menu-heading">
+        <div className="menu-wrapper">
+            <div className = "menu-heading">
                 <h1>Menu</h1>
             </div>
 
-            <div ClassName = "flavors-wrapper">
+            <div className = "flavors-wrapper">
+                <div className = "flav-title">
+                    <h2>Our Flavors</h2>
+                </div>
+
                 
-                <div ClassName = "scoops-wrapper">
-                    <div ClassName = "one-scoop">
+                <div className = "scoops-wrapper">
+                    <div className = "one-scoop">
                         <p>One Scoop - $4.00</p>
                     </div>
 
-                    <div ClassName = "two-scoop">
+                    <div className = "two-scoop">
                         <p>Two Scoops - $5.00</p>
  
                     </div>
 
-                    <div ClassName = "three-scoop">
+                    <div className = "three-scoop">
                         <p>Three Scoops - $6.00</p>
  
                     </div>
 
                 </div>
 
-                <div ClassName = "flavors">
-                    <div ClassName = "flavor">
+                <div className = "flavors">
+                    <div className = "flavor">
                         <p>Triple Berry</p>
                     </div>
 
-                    <div ClassName = "flavor">
+                    <div className = "flavor">
                         <p>Chocolate Mint Oreo</p>
                     </div>
 
-                    <div ClassName = "flavor">
+                    <div className = "flavor">
                         <p>Peach</p>
                     </div>
 
-                    <div ClassName = "flavor">
+                    <div className = "flavor">
                         <p>Strawberry</p>
                     </div>
 
-                    <div ClassName = "flavor">
+                    <div className = "flavor">
                         <p>Raspberry</p>
                     </div>
 
-                    <div ClassName = "flavor">
+                    <div className = "flavor">
                         <p>Carmel Chocolate Pecan</p>
                     </div>
 
-                    <div ClassName = "flavor">
+                    <div className = "flavor">
                         <p>Raspberryt Chocolate Ribbon</p>
                     </div>
 
-                    <div ClassName = "flavor">
+                    <div className = "flavor">
                         <p>Vanilla Almond</p>
                     </div>
 
-                    <div ClassName = "flavor">
+                    <div className = "flavor">
                         <p>Pecan Praline</p>
                     </div>
 
-                    <div ClassName = "flavor">
+                    <div className = "flavor">
                         <p>Eggnog</p>
                     </div>
 
@@ -71,15 +75,66 @@ export default function() {
 
             </div>
 
-            <div ClassName = "topping-wrapper">
-                <div ClassName = "top-price">
+            <div className = "topping-wrapper">
+                <div className = "top-title">
+                    <h2>Our Toppings</h2>
+                </div>
+                <div className = "top-price">
                     <p>Each Topping - Add $.10</p>
 
                 </div>
 
-                <div ClassName = "toppings">
-                    <div ClassName = "topping">
-                        <p></p>
+                <div className = "toppings">
+                    <div className = "topping">
+                        <p>Oreos</p>
+                    </div>
+
+                </div>
+                <div className = "toppings">
+                    <div className = "topping">
+                        <p>Peanut Butter Cup</p>
+                    </div>
+
+                </div>
+                <div className = "toppings">
+                    <div className = "topping">
+                        <p>Gummie Bears</p>
+                    </div>
+
+                </div>
+                <div className = "toppings">
+                    <div className = "topping">
+                        <p>Hot fudge</p>
+                    </div>
+
+                </div>
+                <div className = "toppings">
+                    <div className = "topping">
+                        <p>Caramel Sauce</p>
+                    </div>
+
+                </div>
+                <div className = "toppings">
+                    <div className = "topping">
+                        <p>Cookie Dough</p>
+                    </div>
+
+                </div>
+                <div className = "toppings">
+                    <div className = "topping">
+                        <p>Whipped Cream and Cherry</p>
+                    </div>
+
+                </div>
+                <div className = "toppings">
+                    <div className = "topping">
+                        <p>Strawberries</p>
+                    </div>
+
+                </div>
+                <div className = "toppings">
+                    <div className = "topping">
+                        <p>Mixed Berries</p>
                     </div>
 
                 </div>
