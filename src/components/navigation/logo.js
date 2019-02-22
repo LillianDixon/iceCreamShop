7f103fd661337e3cf2313a33e49e6c4e55523b4d
Lillian Dixon <dixon.lillian92@gmail.com>
2019-02-22 13:00:39 -0700

    commiting before master pull

---
+++ src/components/navigation/logo.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function() {
+    return (
+        <div className="logo">
+            <img src="https://files.slack.com/files-pri/T208051T5-FGDNRC7B2/monty_s_ice_cream_logo_.png" alt="Logo"/>
+        </div>
+    )
+}
--- src/components/pages/menu.js
+++ src/components/pages/menu.js
@@ -3,7 +3,89 @@ import React from 'react'
 export default function() {
     return (
         <div class="menu-wrapper">
-            Menu
+            <div ClassName = "menu-heading">
+                <h1>Menu</h1>
+            </div>
+
+            <div ClassName = "flavors-wrapper">
+                
+                <div ClassName = "scoops-wrapper">
+                    <div ClassName = "one-scoop">
+                        <p>One Scoop - $4.00</p>
+                    </div>
+
+                    <div ClassName = "two-scoop">
+                        <p>Two Scoops - $5.00</p>
+ 
+                    </div>
+
+                    <div ClassName = "three-scoop">
+                        <p>Three Scoops - $6.00</p>
+ 
+                    </div>
+
+                </div>
+
+                <div ClassName = "flavors">
+                    <div ClassName = "flavor">
+                        <p>Triple Berry</p>
+                    </div>
+
+                    <div ClassName = "flavor">
+                        <p>Chocolate Mint Oreo</p>
+                    </div>
+
+                    <div ClassName = "flavor">
+                        <p>Peach</p>
+                    </div>
+
+                    <div ClassName = "flavor">
+                        <p>Strawberry</p>
+                    </div>
+
+                    <div ClassName = "flavor">
+                        <p>Raspberry</p>
+                    </div>
+
+                    <div ClassName = "flavor">
+                        <p>Carmel Chocolate Pecan</p>
+                    </div>
+
+                    <div ClassName = "flavor">
+                        <p>Raspberryt Chocolate Ribbon</p>
+                    </div>
+
+                    <div ClassName = "flavor">
+                        <p>Vanilla Almond</p>
+                    </div>
+
+                    <div ClassName = "flavor">
+                        <p>Pecan Praline</p>
+                    </div>
+
+                    <div ClassName = "flavor">
+                        <p>Eggnog</p>
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div ClassName = "topping-wrapper">
+                <div ClassName = "top-price">
+                    <p>Each Topping - Add $.10</p>
+
+                </div>
+
+                <div ClassName = "toppings">
+                    <div ClassName = "topping">
+                        <p></p>
+                    </div>
+
+                </div>
+
+            </div>
+
         </div>
     )
 }
