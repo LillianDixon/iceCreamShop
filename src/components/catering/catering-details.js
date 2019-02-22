23a7286caf7cafd7a48b73d89166dc76e107466d
hunterbowcutt <jhunterb789@gmail.com>
2019-02-22 12:49:09 -0700

    added catering paragraph

---
+++ src/components/catering/catering-details.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function() {
+    return (
+        <div className="catering-paragraph">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, autem dicta! Amet, est, dolorem illum laudantium aliquid sint soluta, ipsam blanditiis id expedita quae a maxime sed facilis ducimus debitis.
+            </p>
+        </div>
+    )
+}
--- src/components/pages/catering.js
+++ src/components/pages/catering.js
@@ -1,9 +1,25 @@
 import React from 'react'
 
+import CateringDetails from "../catering/catering-details";
+import CateringHeading from "../catering/catering-heading";
+
 export default function() {
     return (
         <div class="catering-wrapper">
             Catering
+
+            <div className="catering-heading">
+                <CateringHeading />
+            </div>
+
+            <div className="top-catering-p">
+                <CateringDetails />
+            </div>
+
+            <div className="bottom-catering-p">
+                <CateringDetails />
+            </div>
+            
         </div>
     )
 }
