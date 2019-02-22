799a22864b1fde7109b4b2876fcab2d7acf2046e
alexjones <ajonzy@gmail.com>
2019-02-22 12:10:24 -0700

    Updated history bio

--- src/components/history/bio.js
+++ src/components/history/bio.js
@@ -4,7 +4,10 @@ export default function() {
     return (
         <div className="history-bio">
             <h2>About Monty's Ice Cream</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe optio itaque nihil eius temporibus libero reiciendis voluptatem inventore excepturi ea eaque, mollitia aliquid est vero expedita molestias fugiat. Nobis quo labore voluptas assumenda. Tempora, praesentium. Sunt hic totam eius ipsum doloribus nostrum fugit beatae earum et, qui distinctio soluta exercitationem.</p>
+            <p>Growing up in Northern California, summer holidays always included my mother’s freshly made homemade ice cream shared on the back patio with family and friends. Ingredients were sourced from my father’s prolific peach tree and the strawberry patch down the hill.</p>
+            <p>My parents homemade ice cream was always dished strait from the mixing can with soft serve consistency. When homemade ice cream was out of season, store bought ice cream was readily available in the kitchen freezer, but it was the homemade creamy confection that we most anticipated.</p>
+            <p>Later, when my wife and I received an ice cream mixer as a wedding present, we knew the Montague tradition of summer ice cream would be perpetuated to another generation. However, we enjoyed the homemade treat so much, the tradition soon extended to fall and winter holidays.</p>
+            <p>Over the years, ice cream has gradually been removed from the family grocery list, with fresh local cream taking its place. I make a batch of ice cream for the family nearly every week, but my children look forward to their birthdays the most when they “order” their own custom flavors.</p>
         </div>
     )
 }
